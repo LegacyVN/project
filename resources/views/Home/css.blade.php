539bae4f7ef602822f68ecc2ec81d3ece39acdc0
pokecryst <pokecryst@gmail.com>
2024-10-01 16:05:50 +0700

    Add files via upload

---
+++ resources/views/Home/css.blade.php
@@ -0,0 +1,33 @@
+<meta charset="utf-8">
+    <title>Foody - Organic Food Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="{{asset('user')}}/img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Lora:wght@600;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" 0>
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{asset('user')}}/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="{{asset('user')}}/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('user')}}/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{asset('user')}}/css/style.css" rel="stylesheet">
+    <!-- <link href="{{asset('user')}}/css/main_yummy.css" rel="stylesheet"> -->
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('user')}}/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="{{asset('user')}}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
