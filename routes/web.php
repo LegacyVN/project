8fa8edd1bff38c622f50709d8addfe4591c5f2d6
Phuong <pokecryst@gmail.com>
2024-10-01 14:11:06 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeUserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
