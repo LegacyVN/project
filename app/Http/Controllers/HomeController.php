143ede1de70bebae37a897b53bfe896716eed2f0
an <thienan09052005@gmail.com>
2024-10-01 15:37:09 +0700

    home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        return view('admin.index');
+    }
+
+
+    public function home(){
+    
+        return view("home/index");
+  
+     }
+
+    // public function home() {
+    //     $product = Product::orderBy('id', 'desc')->take(12)->get();
+    //     return view('home.index' , compact('product'));
+    // }
+
+    public function product_details($id) {
+        $data = Product::find($id);
+        
+        $products = Product::where('category_id', $data->category_id)->take(4)->get();
+        
+        return view('home.product_details', compact('data'), compact('products'));
+    }
+}
